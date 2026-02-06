160c8002cb08bf386f2292d6e7e845f5cf866d35
jwryu765 <jhryu7658@gmail.com>
2026-02-06 17:23:50 +0900

    5일차 커밋 집에가자!!

---
+++ Day05/Chap10-Solution/260206 기초 프로그래밍 언어/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+//int main(void) {
+//	int a = 10;
+//	int b = a++ + 5;
+//
+//	printf("a = %d, b = %d\n", a, b);
+//	return 0;
+//}
+
+int main(void) {
+	int x = 7;
+
+	if (x % 2 == 0)
+		printf("A");
+	else if (x > 5)
+		printf("B");
+	else
+		printf("C");
+
+	return 0;
+}
